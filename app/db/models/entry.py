86faf7fced587fb7518b8ce24ff06ec684bfcb7e
akadir <abdikadir.aweys@gmail.com>
2026-08-17 17:44:24 +0100

    day 13: entry model

---
+++ app/db/models/entry.py
@@ -0,0 +1,17 @@
+from datetime import date, datetime
+
+from sqlalchemy import String, Text, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.db.base import Base
+
+
+class Entry(Base):
+    __tablename__ = "entries"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(200))
+    content: Mapped[str] = mapped_column(Text)
+    mood: Mapped[int]
+    entry_date: Mapped[date]
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
